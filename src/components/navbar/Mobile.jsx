0ead42f6d6f10f9836f25982265f6d42f65ef1ec
Samandar Turg'unboev <123394292+SamandarTurgunboyev@users.noreply.github.com>
2023-09-08 21:34:25 +0500

    navbar

--- src/components/navbar/Mobile.jsx
+++ src/components/navbar/Mobile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
 
-const Mobile = ({ open }) => {
+const Mobile = ({ open, lang, btnLang }) => {
   const { t } = useTranslation()
   return (
     <>
@@ -18,7 +18,7 @@ const Mobile = ({ open }) => {
             }}
           >
             <ul
-              className={`w-[90%] overflow-hidden max-w-[800px] mx-auto bg-white duration-300 box-shadow`}
+              className={`w-[90%] overflow-hidden max-w-[800px] mx-auto bg-white duration-300 box-shadow justify-center flex flex-col items-center gap-[10px]`}
             >
               <li
                 className="text-black text-center cursor-pointer border hover:bg-[#f2f2f2] border-t-0 border-l-0 border-r-0 last:border-b-0"
@@ -34,6 +34,14 @@ const Mobile = ({ open }) => {
                 className="text-black text-center cursor-pointer border hover:bg-[#f2f2f2] border-t-0 border-l-0 border-r-0 last:border-b-0"
               >
                 {t("navbar.link.connection")}
+              </li>
+              <li>
+                <button
+                  className="button-hover border-[#dc4298] text-[#dc4298] w-[80px] h-[60px] rounded-xl border border-sky-500 flex flex-row items-center justify-center"
+                  onClick={() => btnLang(!lang)}
+                >
+                  {lang ? "Uz" : "Ru"}
+                </button>
               </li>
             </ul>
           </motion.div>
